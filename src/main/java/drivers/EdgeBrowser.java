23ef91955db4032a71860921d60c524ff0f76d23
Pradeep Pandey <pradeeppandey70@gmail.com>
2026-05-30 23:19:04 +0530

    configured edgeOptions

--- src/main/java/drivers/EdgeBrowser.java
+++ src/main/java/drivers/EdgeBrowser.java
@@ -2,15 +2,20 @@ package drivers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import options.BrowserOptions;
+import options.EdgeOptionsManager;
 
 public class EdgeBrowser implements BrowserFactory {
 
 	@Override
 	public WebDriver createDriver() {
 		WebDriverManager.edgedriver().setup();
-		return new EdgeDriver();
+		BrowserOptions<EdgeOptions>optionsManager = new EdgeOptionsManager();
+		EdgeOptions options = optionsManager.getOptions();
+		return new EdgeDriver(options);
  
 	}
 
